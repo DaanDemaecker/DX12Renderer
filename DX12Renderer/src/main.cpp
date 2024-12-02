88c8ea5cd1192bd1adf52f24a9f3e4c9dfa0aafe
Daan Demaecker <75164311+DaanDemaecker@users.noreply.github.com>
2024-12-02 22:10:55 +0100

    Added directX includes

--- DX12Renderer/src/main.cpp
+++ DX12Renderer/src/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#define WIN_32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <shellapi.h> // For CommandLineToArgvW
+
+// The min/max macros conflict with like-named member functions.
+// Only use std::min and std::max defined in <algorith>
+#if defined(min)
+#undef min
+#endif
+
+#if defined(max)
+#undef max
+#endif
+
+// In order to define a function called CreateWindow, the Windows macro needs to be undifined
+#if defined(CreateWindow)
+#undef CreateWindow
+#endif
+
+// Windows runtime library. Needed for Microsoft::WRL::CompPtr<> template class
+#include <wrl.h>
+using namespace Microsoft::WRL;
 
 void main()
 {
-    std::cout << "Hello world \n";
+
 }
