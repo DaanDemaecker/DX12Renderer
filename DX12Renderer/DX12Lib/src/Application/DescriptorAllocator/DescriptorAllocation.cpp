cf64b5555dffba691dc0b2b46f31847abc7ae301
Daan Demaecker <75164311+DaanDemaecker@users.noreply.github.com>
2025-04-25 22:16:47 +0200

    Set up DescriptorAllocation header

--- DX12Renderer/DX12Lib/src/Application/DescriptorAllocator/DescriptorAllocation.cpp
+++ DX12Renderer/DX12Lib/src/Application/DescriptorAllocator/DescriptorAllocation.cpp
@@ -3,10 +3,50 @@
 // Header include
 #include "DescriptorAllocation.h"
 
+// File includes
+#include "DescriptorAllocatorPage.h"
+
 DDM::DescriptorAllocation::DescriptorAllocation()
 {
 }
 
+DDM::DescriptorAllocation::DescriptorAllocation(D3D12_CPU_DESCRIPTOR_HANDLE descriptor, uint32_t numHandles, uint32_t descriptorSize, std::shared_ptr<DescriptorAllocatorPage> page)
+{
+}
+
 DDM::DescriptorAllocation::~DescriptorAllocation()
+{
+}
+
+DDM::DescriptorAllocation::DescriptorAllocation(DescriptorAllocation&& allocation)
+{
+}
+
+//DDM::DescriptorAllocation& DDM::DescriptorAllocation::operator=(DescriptorAllocation&& other)
+//{
+//	return nullptr;
+//}
+
+bool DDM::DescriptorAllocation::IsNull() const
+{
+	return false;
+}
+
+D3D12_CPU_DESCRIPTOR_HANDLE DDM::DescriptorAllocation::GetDescriptorHandle(uint32_t offset) const
+{
+	return D3D12_CPU_DESCRIPTOR_HANDLE();
+}
+
+uint32_t DDM::DescriptorAllocation::GetNumHandles() const
+{
+	return 0;
+}
+
+std::shared_ptr<DDM::DescriptorAllocatorPage> DDM::DescriptorAllocation::GetDescriptorAllocatorPage() const
+{
+	return std::shared_ptr<DescriptorAllocatorPage>();
+}
+
+void DDM::DescriptorAllocation::Free()
 {
 }
--- DX12Renderer/DX12Lib/src/Application/DescriptorAllocator/DescriptorAllocation.h
+++ DX12Renderer/DX12Lib/src/Application/DescriptorAllocator/DescriptorAllocation.h
@@ -3,22 +3,63 @@
 #ifndef _DESCRIPTOR_ALLOCATION_
 #define _DESCRIPTOR_ALLOCATION_
 
+// File includes
+#include "Includes/DirectXIncludes.h"
+
+// Standard library includes
+#include <cstdint>
+#include <memory>
+
 namespace DDM
 {
+	// Class forward declarations
+	class DescriptorAllocatorPage;
+
 	class DescriptorAllocation final
 	{
 	public:
+		// Creates a NULL Descriptor
 		DescriptorAllocation();
+
+		DescriptorAllocation(D3D12_CPU_DESCRIPTOR_HANDLE descriptor, uint32_t numHandles, uint32_t descriptorSize, std::shared_ptr<DescriptorAllocatorPage> page);
+
+
+		// Destructor will automatically free allocation
 		~DescriptorAllocation();
 
-		DescriptorAllocation(DescriptorAllocation& other) = delete;
-		DescriptorAllocation(DescriptorAllocation&& other) = delete;
+		// Copies are not allowed.
+		DescriptorAllocation(const DescriptorAllocation&) = delete;
+		DescriptorAllocation& operator=(const DescriptorAllocation&) = delete;
 
-		DescriptorAllocation& operator=(DescriptorAllocation& other) = delete;
-		DescriptorAllocation& operator=(DescriptorAllocation&& other) = delete;
+		// Move is allowed.
+		DescriptorAllocation(DescriptorAllocation&& allocation);
+		DescriptorAllocation& operator=(DescriptorAllocation&& other);
 
+		// Check if this a valid descriptor.
+		bool IsNull() const;
+
+		// Get a descriptor at a particular offset in the allocation.
+		D3D12_CPU_DESCRIPTOR_HANDLE GetDescriptorHandle(uint32_t offset = 0) const;
+
+		// Get the number of (consecutive) handles for this allocation.
+		uint32_t GetNumHandles() const;
+
+		// Get the heap that this allocation came from.
+		// (For internal use only).
+		std::shared_ptr<DescriptorAllocatorPage> GetDescriptorAllocatorPage() const;
 	private:
+		// Free the descriptor back to the heap it came from.
+		void Free();
 
+		// The base descriptor.
+		D3D12_CPU_DESCRIPTOR_HANDLE m_Descriptor;
+		// The number of descriptors in this allocation.
+		uint32_t m_NumHandles;
+		// The offset to the next descriptor.
+		uint32_t m_DescriptorSize;
+
+		// A pointer back to the original page where this allocation came from.
+		std::shared_ptr<DescriptorAllocatorPage> m_Page;
 	};
 }
 
