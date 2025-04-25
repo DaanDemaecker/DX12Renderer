b31b846b474a6a78fdb60f4b78a8dadfef03ee51
Daan Demaecker <75164311+DaanDemaecker@users.noreply.github.com>
2025-04-25 22:23:16 +0200

    Set up DescriptorAllocatorPage header

--- DX12Renderer/DX12Lib/src/Application/DescriptorAllocator/DescriptorAllocatorPage.cpp
+++ DX12Renderer/DX12Lib/src/Application/DescriptorAllocator/DescriptorAllocatorPage.cpp
@@ -3,10 +3,52 @@
 // Header include
 #include "DescriptorAllocatorPage.h"
 
-DDM::DescriptorAllocatorPage::DescriptorAllocatorPage()
+DDM::DescriptorAllocatorPage::DescriptorAllocatorPage(D3D12_DESCRIPTOR_HEAP_TYPE type, uint32_t numDescriptors)
 {
 }
 
 DDM::DescriptorAllocatorPage::~DescriptorAllocatorPage()
 {
 }
+
+D3D12_DESCRIPTOR_HEAP_TYPE DDM::DescriptorAllocatorPage::GetHeapType() const
+{
+	return D3D12_DESCRIPTOR_HEAP_TYPE();
+}
+
+bool DDM::DescriptorAllocatorPage::HasSpace(uint32_t numDescriptors) const
+{
+	return false;
+}
+
+uint32_t DDM::DescriptorAllocatorPage::NumFreeHandles() const
+{
+	return 0;
+}
+
+DDM::DescriptorAllocation DDM::DescriptorAllocatorPage::Allocate(uint32_t numDescriptors)
+{
+	return DescriptorAllocation();
+}
+
+void DDM::DescriptorAllocatorPage::Free(DescriptorAllocation&& descriptorHandle, uint64_t frameNumber)
+{
+}
+
+void DDM::DescriptorAllocatorPage::ReleaseStaleDescriptors(uint64_t frameNumber)
+{
+}
+
+uint32_t DDM::DescriptorAllocatorPage::ComputeOffset(D3D12_CPU_DESCRIPTOR_HANDLE handle)
+{
+	return 0;
+}
+
+void DDM::DescriptorAllocatorPage::AddNewBlock(uint32_t offset, uint32_t numDescriptors)
+{
+}
+
+void DDM::DescriptorAllocatorPage::FreeBlock(uint32_t offset, uint32_t numDescriptors)
+{
+}
+
